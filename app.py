3c00641e4c0837cd3a3edede945d8cc4abbaac16
Kristiaan Pretorius <kristiaanpretorius@hotmail.com>
2019-04-09 12:20:26 +0200

    added app.py to repo

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import jsonify
+from flask import Flask, request, jsonify, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+
+if __name__ == '__main__':
+    app.run()
