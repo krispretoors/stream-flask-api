8a3ccf54d771bb790e6f7ea67c33d6b1e129db50
Kristiaan Pretorius <kristiaanpretorius@hotmail.com>
2019-04-09 14:50:03 +0200

    finished writing script that posts data to endpoint in app.py

--- generate_data.py
+++ generate_data.py
@@ -1,10 +1,34 @@
 import names
 import random
+import json
+import requests
+
+r = random.SystemRandom()
+url = 'http://127.0.0.1:5000/data/persist'
+header = {'content-type': 'application/json'}
 
 def generate_data(list):
     names_total = int(list[0])
     tests_total = int(list[1])
-    
+    payload = { "students": []}
+    rng = random.Random(42)
+
+    for x in range(names_total):
+        first_name = names.get_first_name()
+        last_name = names.get_last_name()
+        payload['students'].append( {
+            "firstName": first_name,
+            "lastName": last_name,
+            "tests": {}
+        } )
+
+        test_var = payload['students'][x]['tests']
+        for y in range(tests_total):
+            test_result = rng.randint(25,101)
+            test_score_val = "test" + str(y + 1)
+            test_var[test_score_val] = test_result
+
+    return json.dumps(payload)
 
 def get_user_input():
     params = []
@@ -19,7 +43,8 @@ def get_user_input():
 def main():
     data = get_user_input()
     payload = generate_data(data)
-    
+
+    response = requests.post(url,json=payload,headers=header)
 
 if __name__ == '__main__':
     main()
