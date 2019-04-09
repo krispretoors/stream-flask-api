9804a367bb72ae0344b959ecf8165b2973fe848c
Kristiaan Pretorius <kristiaanpretorius@hotmail.com>
2019-04-09 22:46:35 +0200

    added finishing touches for review

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
+import json
 
 from flask import jsonify
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from 
 
 app = Flask(__name__)
 
@@ -12,37 +12,36 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import Student, TestResult
+from businesslogic import DatabaseCalls
 
 @app.route("/")
-def hello():
-    return "hello world"
+def welcome():
+    return render_template('index.html')
 
-@app.route("/data/persist",methods=['POST','GET'])
-def persist_data_to_db():
+@app.route("/marks")
+def display_marks():
+    return render_template("marks.html")
+
+@app.route("/getdata")
+def get_test_data():
+    try:
+        result = db.engine.execute("SELECT COUNT(DISTINCT test) FROM testresults;")
+        print(result)
+        tests = User.query.order_by(tests='test').all()
+        return  jsonify([e.serialize() for e in books])
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/data/persist",methods=['POST'])
+def payload_handler():
     jsonData = request.get_json()
+    buslogic = DatabaseCalls(jsonData)
+    if request.method == 'POST':
+        is_persisted = buslogic.persist_data_to_db(jsonData)
+        if is_persisted:
+            return jsonify(success=True, data=jsonData)
+        else:
+            return jsonify(success=False)
 
-    student_list = jsonData['students']
-
-    for student in student_list:
-        test_list = student['tests']
-        student = Student(
-            name = student['firstName'],
-            surname = student['lastName']
-        )
-        db.session.add(student)
-        db.session.commit()
-
-        print(student.id)
-        for key, value in test_list.items():
-            student_test = TestResult(
-                test = key,
-                result = value,
-                student_id = student.id
-            )
-            db.session.add(student_test)
-            db.session.commit()
-
-    return jsonify(success=True, data=jsonData)
-    
 if __name__ == '__main__':
     app.run()
--- businesslogic.py
+++ businesslogic.py
@@ -1,2 +1,29 @@
-def world():
-    return 'hello world'
+from models import Student, TestResult
+
+class DatabaseCalls:
+    def __init__(self, payload):
+        self.payload = payload
+
+    def persist_data_to_db(self, payload):
+        try:
+            newd = json.loads(self.payload)
+            student_list = newd['students']
+    
+            for student in student_list:
+                test_list = student['tests']
+                student = Student(
+                    name = student['firstName'],
+                    surname = student['lastName']
+                )
+                db.session.add(student)
+
+                for key, value in test_list.items():
+                    student_test = TestResult(
+                        test = key,
+                        result = value,
+                        student_id = student.id
+                    )
+                    db.session.add(student_test)
+                    db.session.commit()
+        except Exception as e:
+            return(str(e))
